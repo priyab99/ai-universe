54dfb06ad869e569065ad53e4ba79dd4244e29cc
priyab99 <priya.bd.9911@gmail.com>
2023-03-02 22:08:31 +0600

    all apis is showing

--- js/universe.js
+++ js/universe.js
@@ -2,31 +2,33 @@ const loadApi=()=>{
     const url=`https://openapi.programming-hero.com/api/ai/tools`
     fetch(url)
     .then(res=>res.json())
-    .then(data=>displayApi(data))
+    .then(data=>displayApi(data.data.tools))
 }
 
-const displayApi=(...apis)=>{
+const displayApi=(apis)=>{
     const apisContainer=document.getElementById('apis-container');
     //display 6 api only
+    const showAll=document.getElementById('show-all')
     //apisContainer.textContent='';
+   if(Array.isArray(apis)){
     apis.forEach(api => {
         const apiDiv=document.createElement('div');
         apiDiv.classList.add('col');
         apiDiv.innerHTML= `
         <div class="col">
         <div class="card h-100">
-          <img src="${api.data.tools[0].image}" class="card-img-top" alt="...">
+          <img src="${api.image}" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Features</h5>
             <p class="card-text"><ol class="list-group list-group-numbered">
-            <li class="list-group-item">${api.data.tools[0].features[0]}</li>
-            <li class="list-group-item">${api.data.tools[0].features[1]}</li>
-            <li class="list-group-item">${api.data.tools[0].features[2]}</li>
+            <li class="list-group-item">${api.features[0]}</li>
+            <li class="list-group-item">${api.features[1]}</li>
+            <li class="list-group-item">${api.features[2]}</li>
           </ol></p>
           </div>
           <div class="card-footer">
-            <h6>${api.data.tools[0].name}</h6>
-            <p>${api.data.tools[0].published_in}</p>
+            <h6>${api.name}</h6>
+            <p>${api.published_in}</p>
           </div>
         </div>
       </div>
@@ -36,6 +38,7 @@ const displayApi=(...apis)=>{
         apisContainer.appendChild(apiDiv);
         
     });
+   }
 }
 
 
