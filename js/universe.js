99f3c7dc0a91af23ab15b2a595975ba502cb6259
priyab99 <priya.bd.9911@gmail.com>
2023-03-04 15:55:11 +0600

    see more button working

--- js/universe.js
+++ js/universe.js
@@ -1,4 +1,4 @@
-const loadApi=(res)=>{
+const loadApi=()=>{
     const url=`https://openapi.programming-hero.com/api/ai/tools`
     fetch(url)
     .then(res=>res.json())
@@ -70,14 +70,71 @@ const displayApi=(apis)=>{
     hideSpinner(false);
    }
 }
-//see more
+//see more 
 document.getElementById('btn-see-more').addEventListener('click',function(){
     const url=`https://openapi.programming-hero.com/api/ai/tools`
     fetch(url)
     .then(res=>res.json())
-    .then(data=>displayApi(data.data.tools))
+    .then(data=>displayAllApi(data.data.tools))
 
 })
+//this is the function for showing all api cards
+
+const displayAllApi=(apis)=>{
+  const apisContainer=document.getElementById('apis-container');
+   //apisContainer.textContent='';
+   if(Array.isArray(apis)){
+    apis.forEach(api => {
+        const apiDiv=document.createElement('div');
+        apiDiv.classList.add('col');
+        apiDiv.innerHTML= `
+        <div class="col">
+        <div class="card h-100">
+          <img src="${api.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Features</h5>
+            
+            <p class="card-text"><ol class="list-group list-group-numbered">
+            <li class="list-group-item">${api.features[0]}</li>
+            <li class="list-group-item">${api.features[1]}</li>
+            <li class="list-group-item">${api.features[2]}</li>
+          </ol></p>
+          </div>
+          <div class="card-footer d-flex justify-content-between">
+            <div>
+            <h6>${api.name}</h6>
+            <div class="d-flex">
+            <div>
+            <i class="fa-solid fa-calendar-days mx-2"></i>
+            </div>
+            <div class="mx-2">
+            <p>${api.published_in}</p>
+            </div>
+            </div>
+
+            <div>
+            <i onclick="loadApiDetails('${api.id}')" class="fa-solid fa-arrow-right" data-bs-toggle="modal" data-bs-target="#apiDetailModal"></i>
+            </div>
+           
+          </div>
+         
+         
+        </div>
+      </div>
+
+
+        `
+        apisContainer.appendChild(apiDiv);
+
+        
+    });
+    //stop loader
+    hideSpinner(false);
+   }
+   const btnSeeMore=document.getElementById('btn-see-more');
+   btnSeeMore.style.visibility='hidden';
+
+}
 
 //api detail fetching
 const loadApiDetails=(id)=>{
@@ -94,27 +151,26 @@ const displayApiDetails=api=>{
     const apiCardDetal=document.getElementById('modal-details')
     const modalDetailOne=document.getElementById('modal-details-one');
 
-    // Get the features object from the API
    const features = api.features;
 
-   // Extract the feature names from the features object and store them in an array
+   
    const featureNames = Object.values(features).map(feature => feature.feature_name);
   
     modalDetailOne.innerHTML=`
 
-    <p class="text-black fw-bold">${api.description}  <p>
+    <p class="text-black fw-bold mt-3">${api.description}  <p>
      <div class="d-flex juatify-content-between">
 
      <div class="mx-3 text-success">
-     <p>${api.pricing[0].price}</p>
+     <p>${api.pricing[0].price?api.pricing[0].price: "Free Of Cost"}</p>
      <p>${api.pricing[0].plan}</p>
      </div>
      <div class="mx-3 text-warning">
-     <p>${api.pricing[1].price}</p>
+     <p>${api.pricing[1].price? api.pricing[1].price: "Free of Cost"}</p>
      <p>${api.pricing[1].plan}</p>
      </div>
      <div class="mx-3 text-danger">
-     <p>${api.pricing[2].price}</p>
+     <p>${api.pricing[2].price? api.pricing[2].price: "Free of Cost"}</p>
      <p>${api.pricing[2].plan}</p>
      </div>
 
@@ -136,10 +192,10 @@ const displayApiDetails=api=>{
     <h5 class="card-title">Intregations</h5>
           
     <ul>
-    <li>${api.integrations[0]}</li>
-    <li>${api.integrations[1]}</li>
-    <li>${api.integrations[2]}</li>
-  </ul></p>
+    <li>${api.integrations[0]?api.integrations[0]: "No data Found"}</li>
+    <li>${api.integrations[1]?api.integrations[1]: "No data Found"}</li>
+    <li>${api.integrations[2]?api.integrations[2]: "No data Found"}</li>
+  </ul>
     </div>
     </div>
     `
@@ -149,12 +205,12 @@ const displayApiDetails=api=>{
     const modalDetailTwo=document.getElementById('modal-details-two');
     modalDetailTwo.innerHTML=`
     <div style="position: relative;">
-    <img src="${api.image_link[0]}" class="rounded mx-auto d-block" style="max-width: 100%;" alt="...">
-    <button class="btn btn-danger" style="position: absolute; top: 0; right: 0;">${api.accuracy.score*100}% accuracy</button>
+    <img src="${api.image_link[0]}" class="rounded mx-auto d-block mt-3" style="max-width: 100%;" alt="...">
+    <button class="btn btn-danger" style="position: absolute; top: 0; right: 0;">${api.accuracy.score ? api.accuracy.score*100 + "% accuracy" : ""}</button>
   </div>
   
-    <h3>${api.input_output_examples[0].input?api.input_output_examples[0].input:api.input_output_examples[1].input}</h3>
-    <p>${ api.input_output_examples[0].output?api.input_output_examples[0].output:api.input_output_examples[1].output}</p>
+    <h3 class="text-center">${api.input_output_examples[0].input?api.input_output_examples[0].input:api.input_output_examples[1].input}</h3>
+    <p class="text-center">${ api.input_output_examples[0].output?api.input_output_examples[0].output:api.input_output_examples[1].output}</p>
     `
     
 
